0616f51bd88da5f9282bb74d01472266b5683224
Alfredo Oviedo <alfredo.g.oviedo08@gmail.com>
2022-06-22 17:06:05 -0500

    completed SpeedConverter.java

--- conversion/Main.java
+++ conversion/Main.java
@@ -2,7 +2,10 @@ package conversion;
 
 public class Main {
 
-
+    public static void main(String[] args) {
+        long miles = SpeedConverter.toMilesPerHour(10.5);
+        System.out.println("Miles " + miles);
+        SpeedConverter.printConversion(10.5);
     }
 
 }

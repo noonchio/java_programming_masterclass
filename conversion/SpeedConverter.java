f202369e4ab98dd2455502d1800a7c16c65ee0fa
Alfredo Oviedo <alfredo.g.oviedo08@gmail.com>
2022-06-22 16:19:46 -0500

    add Main.java

---
+++ conversion/SpeedConverter.java
@@ -0,0 +1,14 @@
+package conversion;
+
+public class SpeedConverter {
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+
+        if (kilometersPerHour < 0) {
+            return -1;
+        }
+
+        return Math.round(kilometersPerHour /1.609);
+    }
+
+}

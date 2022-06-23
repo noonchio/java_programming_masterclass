15ca1e60a0a408f8c11bd525ebc641805b0aac90
Alfredo Oviedo <alfredo.g.oviedo08@gmail.com>
2022-06-23 11:43:06 -0500

    add EqualSumChecker.java

---
+++ EqualSumChecker.java
@@ -0,0 +1,12 @@
+public class EqualSumChecker {
+    // The method should return boolean and it needs to return true if the sum of the first and second parameter is equal to the third parameter.Otherwise, return false.
+
+    public static boolean hasEqualSum(int a, int b, int c){
+        if(a + b == c){
+            return true;
+        }
+        else{
+          return false;
+        }
+    }
+}
